1f224965997004cb014dfc432592e3c5c4048ea9
gretchenau <gretchenaussie@gmail.com>
2021-02-16 20:37:19 +0300

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle} from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
 
     function RenderCard({item, isLoading, errMess}) {
@@ -19,6 +20,11 @@ import { baseUrl } from '../shared/baseUrl';
         }
         else 
             return(
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
                 <Card>
                     <CardImg src={baseUrl + item.image} alt={item.name} />
                     <CardBody>
@@ -27,6 +33,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <CardText>{item.description}</CardText>
                     </CardBody>
                 </Card>
+            </FadeTransform>
             );
     
     }
